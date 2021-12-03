89e6a70990adb36d0b5591aa54757b73d49dc808
mikxingu <michelalvs@gmail.com>
2021-12-03 01:34:09 -0300

    Created database connection classes and exceptions

---
+++ src/db/DatabaseIntegrityException.java
@@ -0,0 +1,9 @@
+package db;
+
+public class DatabaseIntegrityException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseIntegrityException(String message) {
+		super(message);
+	}
+}
